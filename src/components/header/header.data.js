f631174938432b0d9c8a3b9155bea2d401692349
Chomba <dev.chomba@gmail.com>
2021-12-17 20:19:52 +0200

    initial landing page setup.

---
+++ src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Home',
+  },
+  {
+    path: 'services',
+    label: 'Services',
+  },
+  {
+    path: 'about-us',
+    label: 'About us',
+  },
+  {
+    path: 'testimonials',
+    label: 'Contact us',
+  },
+  // {
+  //   path: 'news',
+  //   label: 'News',
+  // },
+];
